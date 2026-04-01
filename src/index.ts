d9998f6b9b7bde60d4c9d0ff5f580abb9d6063a0
devzshrc <de5ash1zh@gmail.com>
2026-04-01 14:07:53 +0530

    server-setup

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,17 @@
-console.log("helloooo");
+import { createServer } from "node:http";
+import { createApplication } from "./app";
+
+async function main() {
+  try {
+    const server = createServer(createApplication());
+    // we have an http server and we want express to handle the routes that's why we've passed createApplication in - as it's an express instance
+    const PORT: number = 8081;
+    server.listen(PORT, () => {
+      console.log(`Http server is running on PORT: ${PORT}`);
+    });
+  } catch (error) {
+    console.log(`Error starting the http server`);
+    throw error;
+  }
+}
+main();
